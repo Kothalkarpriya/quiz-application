f55330cbea2c99779e097ff3d41253ee4a4057b6
Priya Kothalkar <priyakothalkar2973@gmail.com>
2022-05-07 23:09:39 +0530

    feat-navbar and footer added

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import "../assests/styles/navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <header className="nav-brand upper-text">
+        <h1>
+          <Link to="/" className="nav-brand-link">
+            Quiz App
+          </Link>
+        </h1>
+      </header>
+      <div className="nav-collapse">
+        <ul className="nav-list-group">
+          <li className="list-ele">
+            <Link to="/" className="nav-link upper-text">
+              Home
+            </Link>
+          </li>
+          <li className="list-ele">
+            <Link to="/Login" className="nav-link upper-text">
+              Login
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
