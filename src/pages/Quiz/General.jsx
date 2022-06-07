7165f863428f39f0c9b8c586ce83be5e336ea916
Priya Kothalkar <priyakothalkar2973@gmail.com>
2022-06-07 10:35:30 +0530

    rm- unwanted files

--- src/pages/Quiz/General.jsx
+++
@@ -1,5 +0,0 @@
-export default function General(){
-    return(
-        <div>General Quiz</div>
-    )
-}
--- src/pages/Quiz/Logical.jsx
+++
@@ -1,5 +0,0 @@
-export default function Logical(){
-    return(
-        <div>Logical Quiz</div>
-    )
-}
